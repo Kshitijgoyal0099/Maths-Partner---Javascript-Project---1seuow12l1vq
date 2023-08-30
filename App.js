8fc836076794e4f6731583613fe5195b312a911c
Kshitij Goyal <74810318+Kshitijgoyal0099@users.noreply.github.com>
2023-08-30 17:19:38 +0530

    Upload on 30/08/2023

---
+++ App.js
@@ -0,0 +1,90 @@
+//INDEX PAGE JS
+
+let displayResults = document.getElementById("resultDisplay");
+let clearBtn = document.getElementById("clearBtn");
+
+
+let existingSolutions = JSON.parse(window.localStorage.getItem("solutions"));
+
+document.addEventListener("DOMContentLoaded", function() {
+    var getValueButton = document.getElementById("searchResult");
+    getValueButton.addEventListener("click", getInputValue);  
+});
+
+function getInputValue(){
+    let expression = document.getElementById("problemID").value;
+    let operation = document.getElementById("category").value;
+    var encodedExpression = encodeURIComponent(expression);
+    var API= `https://newton.vercel.app/api/v2/${operation}/${encodedExpression}`;
+    console.log("GIV Running: " + API);
+    displayResults.innerHTML = "calculating...";
+    let printOperation = toSentenceCase(operation);
+    fetch(API)
+  .then(response => response.json())
+  .then(data => {
+    const result = data.result;
+    const answer = 
+    ` <h4>${printOperation} : ${expression}</h4>
+      <p>Result : ${result}</p>
+    `;
+    displayResults.innerHTML = answer;
+    updateSolutions(operation,expression,result);
+  })
+  .catch(error => {
+    console.error('An error occurred:',error);
+  });
+}
+
+function toSentenceCase(str){
+  return str.toLowerCase().charAt(0).toUpperCase() + str.slice(1);;
+}
+
+function updateSolutions(ope,exp,res){
+    const tempSol = {
+        "operation":ope,
+        "expression":exp,
+        "result":res
+    };
+    if(existingSolutions){
+      existingSolutions.push(tempSol);
+      window.localStorage.setItem("solutions",JSON.stringify(existingSolutions));
+    }else{
+      window.localStorage.setItem("solutions",JSON.stringify(tempSol));
+    }
+}
+
+// HISTORY PAGE JS
+
+
+let tb = document.getElementById("tableBody");
+
+var i = 0;
+const tContent = existingSolutions.map((e,index) => `
+    <tr>
+      <td>${index+1}</td>
+      <td>${e.expression}</td>
+      <td>${e.operation}</td>
+      <td>${e.result}</td>
+      <td><button class = "delBtn" id = "delBtn-${index} delBtnID"><i class="fa-solid fa-trash-can"></i></button></td>    
+    </tr>
+`).join('');
+
+tb.innerHTML = tContent;
+const container = document.getElementById('tableContent');
+
+container.addEventListener('click', (event) => {
+  if (event.target.classList.contains('delBtn')) {
+    const index = parseInt(event.target.id.split('-')[1]);
+    existingSolutions.splice(index, 1); 
+    window.localStorage.setItem("solutions",JSON.stringify(existingSolutions));
+    
+    location.reload(); 
+
+  }
+});
+
+
+clearBtn.addEventListener("click",(event) =>{
+    displayResults.innerHTML = "";
+});
+
